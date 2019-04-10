c5069ee179d3d0f4e07f76f92ebe0a5d2510008d
Farai Manyanga <fmanyanga00@gmail.com>
2019-04-10 07:39:26 +0000

    oh yeah yeah

--- main.c
+++ main.c
@@ -3,7 +3,28 @@
 int main()
 {
     //Encryption of a message with a rotation cipher given the message text and rotation amount
-    
+    //create switch case to chose action as menu
+    //first case does first mission and so on
+    //ascii used for conversions
+    //blah blah blah
+    //ok lets get coding
+    int action
+    printf("what is your command \n");
+    printf("1: encrypt as task 1 \n");
+    printf("2: drcrypt as task 2 \n");
+    scanf("%d", &action);
+    switch action {
+        case 1{
+            break;
+        }
+        case 2{
+            break;
+        }
+        default {
+            printf("incorrect action input");
+            break;
+        }
+    }
     printf("input text to encrypt please \n");
     scanf("%s", &text);
     encrypt
